a0b40163bb7d4088d56d6c1dacbd9443ce9137f3
Reema S <144640434+rsolan@users.noreply.github.com>
2025-01-26 18:09:32 -0500

    Update 1.basic.py

--- 6.Graph/1.basic.py
+++ 6.Graph/1.basic.py
@@ -4,3 +4,16 @@ class Solution:
         total_edges = (n * (n - 1)) // 2
         # Calculate the number of possible graphs
         return 2 ** total_edges
+
+
+# adjacency list representation
+class Solution:
+    def printGraph(self, V : int, edges : List[List[int]]) -> List[List[int]]:
+        # code here
+        adj_list = [[] for _ in range(V)]
+        
+        for u,v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+            
+        return adj_list
