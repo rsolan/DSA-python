f393d06a2ef71d845d1baabad342ea5ada656b00
Reema S <144640434+rsolan@users.noreply.github.com>
2025-01-29 18:12:45 -0500

    Create 1.Bipartite_bfs_dfs.py

---
+++ 6.Graph/Bipartitie_topo/1.Bipartite_bfs_dfs.py
@@ -0,0 +1,57 @@
+from collections import deque
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        # visite a adj node which is not a parent and already visiste d- issue - cycle detected
+        # https://www.geeksforgeeks.org/problems/bipartite-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bipartite-graph
+
+        # m2 - dfs
+        n  = len(graph)
+        vcolor = [-1 for  i in range(n)]
+
+        def dfs(node,col):
+            vcolor[node] = col
+            for neigh in graph[node]:
+                if vcolor[neigh] == -1: #notvis
+                    if dfs(neigh,not col) == False:  #------vvvvimp dfs calss are returned if false - need tocheck
+                        return False
+
+                elif vcolor[neigh] == vcolor[node]: #neigh visited and has same color as of current node
+                    return False
+
+            return True
+
+        for i in range(n): #for multicomponents
+            if vcolor[i] == -1:
+                if dfs(i,0) == False:   #--sending src with color = 0
+                    return False
+
+        return True
+
+        '''
+        # m1 - using bfs
+        n  = len(graph)
+        vcolor = [-1 for  i in range(n)]
+
+        def bfs(src):
+            q = deque()
+            q.append(src)
+            vcolor[src] = 0
+
+            while q:
+                node = q.popleft()
+               
+                for neigh in graph[node]:
+                    if vcolor[neigh] == -1: #notvis
+                        vcolor[neigh]  = not vcolor[node]
+                        q.append(neigh)
+                    elif vcolor[neigh] == vcolor[node]: #neigh visited and has same color as of current node
+                        return False
+            return True
+
+        for i in range(n): #for multicomponents
+            if vcolor[i] == -1:
+                if bfs(i) == False:
+                    return False
+
+        return True
+        '''
