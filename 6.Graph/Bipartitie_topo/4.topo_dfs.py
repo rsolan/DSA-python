28c84eb57e85cfe7476abf33d6a593ac2dca5dca
Reema S <144640434+rsolan@users.noreply.github.com>
2025-01-29 19:07:25 -0500

    Create 4.topo_dfs.py

---
+++ 6.Graph/Bipartitie_topo/4.topo_dfs.py
@@ -0,0 +1,31 @@
+# https://www.geeksforgeeks.org/problems/topological-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=topological-sort
+
+#Function to return list containing vertices in Topological order.
+    def topologicalSort(self,adj):
+        # Code here
+        # adjacency list for a Directed Acyclic Graph (DAG)
+        
+        # m1 - use dfs - to store ts we use stack
+        # TC - O(V+E) directed graph, se - o(2n) st and vis
+        n = len(adj)
+        st =[]
+        vis =[0 for i in range(n)]
+        def dfs(node):
+            vis[node] = 1
+            for i in adj[node]:
+                if not vis[i]:
+                    dfs(i)
+                    
+            st.append(node)  #-- store node in stack after dfs call while returning
+        
+        
+        for i in range(n):
+            if not vis[i]:
+                dfs(i)
+        
+        ans = []
+        while st:
+            node = st.pop()
+            ans.append(node)
+            
+        return ans
