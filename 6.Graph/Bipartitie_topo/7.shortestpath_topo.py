85fe08eade0c924a19d4b3cea21cbe46ee3a062b
Reema S <144640434+rsolan@users.noreply.github.com>
2025-01-31 18:19:53 -0500

    Update 7.shortestpath_topo.py

--- 6.Graph/Bipartitie_topo/7.shortestpath_topo.py
+++ 6.Graph/Bipartitie_topo/7.shortestpath_topo.py
@@ -2,7 +2,7 @@
 
 '''
 1. adj list with weight
-2. form a topo sort  using dfs stack
-3. take top of stack - for top node
+2. form a topo sort  using dfs stack  -- will give correct order to proces for  o(n+m)
+3. take top of stack - for top node o(n+m)
 d[node] +wt  < d[v] - so update
 '''
