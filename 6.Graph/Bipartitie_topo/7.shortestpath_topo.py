aa1d482969e9b583f28487212382eaa5b2bd275a
Reema S <144640434+rsolan@users.noreply.github.com>
2025-01-31 18:19:12 -0500

    Update 7.shortestpath_topo.py

--- 6.Graph/Bipartitie_topo/7.shortestpath_topo.py
+++ 6.Graph/Bipartitie_topo/7.shortestpath_topo.py
@@ -1,1 +1,8 @@
 # https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=direct-acyclic-graph
+
+'''
+1. adj list with weight
+2. form a topo sort  using dfs stack
+3. take top of stack - for top node
+d[node] +wt  < d[v] - so update
+'''
