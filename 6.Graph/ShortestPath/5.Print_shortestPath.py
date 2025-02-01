090dde8687287f1f9a437730f3e51941897bad58
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-01 08:56:00 -0500

    Update 5.Print_shortestPath.py

--- 6.Graph/ShortestPath/5.Print_shortestPath.py
+++ 6.Graph/ShortestPath/5.Print_shortestPath.py
@@ -1,5 +1,12 @@
+'''
 https://www.geeksforgeeks.org/problems/shortest-path-in-weighted-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=shortest-path-in-weighted-undirected-graph
 updat when dist is less
   store the parent [] to store node for each adj_node
                               then traverse in parent to form a list of list - to return who is coming from wehre
                               start from dest and backtrack
+start from n to till parent become equal to node
+push the src in lat 
+reverse th path 
+
+tc - dijkstra+path = elogv+n
+'''
