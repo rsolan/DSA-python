4f05436fa87b2dd0a64896cc90952947c6a1403b
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-01 15:45:42 -0500

    Update 7.PathMinEffort.py

--- 6.Graph/ShortestPath/7.PathMinEffort.py
+++ 6.Graph/ShortestPath/7.PathMinEffort.py
@@ -14,3 +14,33 @@ while q
 
 O(ELOGV) - 4nm log (nm)
   
+'''
+import heapq
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        n = len(heights)
+        m = len(heights[0])
+    
+        pq=[]
+        heapq.heappush(pq,(0,0,0))  #dist,r,c  - dist i s0
+        
+        dist = [[float('inf')  for _ in range(m)] for _ in range(n)]
+        dist[0][0] = 0
+        delr =[-1,0,1,0]
+        delc =[0,1,0,-1]
+        while pq:
+            d,row,col  = heapq.heappop(pq)
+             # If we've reached the bottom-right corner, return the distance
+            if row == n - 1 and col == m - 1:
+                return d
+
+            for i in range(4):
+                nr = row+delr[i]
+                nc = col+delc[i]
+                if nr>=0 and nr<n and nc>=0 and nc<m:
+                    newEffort = max(abs(heights[nr][nc] - heights[row][col]), d)  #- take max of old and diff
+                    if newEffort <  dist[nr][nc]:  #update for new if max is smaller
+                        dist[nr][nc] = newEffort
+                        heapq.heappush(pq,(newEffort, nr, nc))
+
+        return 0
