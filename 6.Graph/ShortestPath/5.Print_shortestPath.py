2fbbaab7c58b4f68e9b6fcf02c9656455c34ffe1
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-01 14:38:33 -0500

    Update 5.Print_shortestPath.py

--- 6.Graph/ShortestPath/5.Print_shortestPath.py
+++ 6.Graph/ShortestPath/5.Print_shortestPath.py
@@ -10,3 +10,43 @@ reverse th path
 
 tc - dijkstra+path = elogv+n
 '''
+from typing import List
+import heapq
+class Solution:
+    def shortestPath(self,n:int, m:int, edges:List[List[int]] )->List[int]:
+        # code here
+        adj=[[] for _ in range(n+1)]
+        for u,v,wt in edges:
+            adj[u].append((v,wt))
+            adj[v].append((u,wt))  #- imp as undir
+            
+        parent = [i for i in range(n+1)]  #-initialize with i or -1
+        dist = [float('inf') for i in range(n+1)] #1 to n+1
+         
+        pq = []
+        heapq.heappush(pq,(0,1))
+        dist[1] = 0  #- start from 1st node -- 
+        
+        while pq:
+            d,node = heapq.heappop(pq)
+            
+            for v,wt in adj[node]:
+                if d+wt<dist[v]:
+                    dist[v]  = d+wt
+                    parent[v] = node
+                    heapq.heappush(pq,(dist[v],v))
+                    
+        # Path reconstruction from node n to 1
+        if dist[n] == float('inf'):  # No path exists to node n
+            return [-1]  # Return -1 if no path exists
+            
+            
+        ans =[]
+        
+        nd = n  #nd goes from n to 1
+        while nd!=1:
+            ans.append(nd)
+            nd = parent[nd]
+        ans.append(1)  #- src need loop breaks, but has to inc in ans path
+        path = ans[::-1]
+        return [dist[n]]+path
