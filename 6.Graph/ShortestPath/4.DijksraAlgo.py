d076e8829f1ff18ce894bd95d47fe76fc0c1b530
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-01 08:34:16 -0500

    Create 4.DijksraAlgo.py

---
+++ 6.Graph/ShortestPath/4.DijksraAlgo.py
@@ -0,0 +1,8 @@
+'''
+shortest
+using pq - min heap 
+use set -- min at top ie ascending order 
+
+using set - we can remove already existing paths(logn) high 10,5 and add 8,5
+dist,node
+'''https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
