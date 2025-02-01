bbc6a66cfe977ef1979d2e179e1073c6ffe95437
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-01 08:50:06 -0500

    Create 5.Print_shortestPath.py

---
+++ 6.Graph/ShortestPath/5.Print_shortestPath.py
@@ -0,0 +1,5 @@
+https://www.geeksforgeeks.org/problems/shortest-path-in-weighted-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=shortest-path-in-weighted-undirected-graph
+updat when dist is less
+  store the parent [] to store node for each adj_node
+                              then traverse in parent to form a list of list - to return who is coming from wehre
+                              start from dest and backtrack
