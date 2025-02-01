5fb3efcba02deb3c4832fc3db061ee6ccdde8922
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-01 15:57:18 -0500

    Update 8.CheapestFlight.py

--- 6.Graph/ShortestPath/8.CheapestFlight.py
+++ 6.Graph/ShortestPath/8.CheapestFlight.py
@@ -18,3 +18,31 @@ check <=k , till k is allowed staring from 0 as k can be dest
 
 TC = E as onlye q  = flight size
 '''
+from collections import deque
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # imp goal is not cheapest flight 
+        # goal is with k constrant cheapest flight  -- USE QUEUE AND NOT PQ
+
+
+        adj =[[] for _ in range(n)]
+        for u,v,wt in flights:
+            adj[u].append((v,wt))
+
+        q = deque()
+        q.append((0,src,0))  #stops, node, cost
+        dist = [float('inf') for i in range(len(adj))]
+        dist[src] = 0
+
+        while q:
+            stop,node,cost = q.popleft()
+            if stop>k:  # skip teh curr iter if stops>k
+                continue
+            for v,wt in adj[node]:
+                if cost + wt < dist[v] and stop<=k:  #-- imp <= as current dest also incl
+                    dist[v] = cost+wt
+                    q.append((stop+1,v,dist[v]))  #- inc the stop
+
+        if dist[dst] == float('inf'):
+            return -1
+        return dist[dst]
