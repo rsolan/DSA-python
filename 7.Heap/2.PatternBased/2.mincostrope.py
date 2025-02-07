8de168325d573603797740abebe5050c96532586
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-06 23:06:54 -0500

    Create 2.mincostrope.py

---
+++ 7.Heap/2.PatternBased/2.mincostrope.py
@@ -0,0 +1,40 @@
+# https://www.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+
+import heapq
+class Solution:
+    #Function to return the minimum cost of connecting the ropes.
+   def minCost(self, arr: list[int]) -> int:
+    
+        # code here
+        heap =[]
+        ans = 0
+        sumj = 0
+        
+        for i in arr:
+            heapq.heappush(heap,i)
+            
+        while len(heap)>1:
+            m1 = heapq.heappop(heap)
+            m2 = heapq.heappop(heap)
+            
+            sumj = m1+m2
+            
+            # print(heap)
+            if heap:
+                heapq.heappush(heap,sumj)
+            
+            ans = ans+sumj
+        return ans
+        
+        
+        # while heap:
+        #     m1 = heapq.heappop(heap)
+        #     if heap:
+        #         m2 = heapq.heappop(heap)
+        #     sumj = m1+m2
+        #     # print(heap)
+        #     if heap:
+        #         heapq.heappush(heap,sumj)
+           
+        #     ans = ans+sumj
+        # return ans
