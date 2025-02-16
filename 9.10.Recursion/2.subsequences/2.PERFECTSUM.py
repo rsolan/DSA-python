b8dce4230c003dfc421540407d5a27e8c18df34a
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-15 21:36:03 -0500

    Update 2.PERFECTSUM.py

--- 9.10.Recursion/2.subsequences/2.PERFECTSUM.py
+++ 9.10.Recursion/2.subsequences/2.PERFECTSUM.py
@@ -7,22 +7,56 @@ All possible patters from Subsequence
 4. Print the count of Sq which sums to K
 
 
-*Note In order to understand Printing all subsequence in absolute clear way..... Just take the example which striver gave in previous video
+1) COUNT FOR HOW MANY SUM == K
+'''
+        # use - this str fo count
+        if true - retunr 1
+        else - return 0
+        l()
+        r()
+        return l+r
+        
+        
+        if n rec calls - n queen
+        if true - retunr 1
+        else - return 0
+        s = 0
+        for i in n:
+            s+=f()
+        return s
+        '''
+        
 
-Now create a table of all the output and match it with the power set. A magic will happen and you will be totally blown away
-
-
-https://www.geeksforgeeks.org/problems/perfect-sum-problem5633/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=perfect-sum-problem
-TLE
 #User function Template for python3
 class Solution:
 	def perfectSum(self, arr, target):
+	    
 		# code here
+		def rec(ind,arr,s):
+		    n = len(arr)
+		    if ind == n:
+		        if s == target:
+		            return 1
+	            return 0
+		            
+		    s = s+arr[ind]
+		    left = rec(ind+1,arr,s)
+		    
+		    s = s-arr[ind]
+		    right = rec(ind+1,arr,s)
+			
+		    return left+right
+		    
+        s=0
+        return rec(0,arr,s)
+         
+        
+		''' - DONT UES THIS
 		def rec(ind,arr,ans,s,cnt):
 		    n = len(arr)
 		    if ind == n:
 		        if s == target:
-		            cnt+=1
+		            cnt[0]+=1
 	            return
 		            
 		    ans.append(arr[ind])
@@ -33,8 +67,53 @@ class Solution:
 		    ans.pop()
 		    rec(ind+1,arr,ans,s,cnt)
 		    
-        cnt =0
+        cnt =[0]
         ans = []
         s=0
         rec(0,arr,ans,s,cnt)
-        return cnt
+        return cnt[0]
+        '''
+        #
+
+
+
+
+2) RETURN TRUE OR FALSE IF ANY WITH SUM = K
+https://www.naukri.com/code360/problems/subset-sum_630213?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
+
+from typing import *
+
+def isSubsetPresent(n:int, k: int, arr: List[int]) -> bool:
+    # Write your code here.
+    def rec(ind,arr,ans,s):
+        n = len(arr)
+        if ind == n:
+            if s == k:
+                return True
+            return False
+                
+        ans.append(arr[ind])
+        s = s+arr[ind]
+        if rec(ind+1,arr,ans,s):
+            return True
+        rec(ind+1,arr,ans,s)
+        
+        s = s-arr[ind]
+        ans.pop()
+        if rec(ind+1,arr,ans,s):
+            return True
+        # rec(ind+1,arr,ans,s)
+
+        return False
+		    
+    
+    ans = []
+    s=0
+    if rec(0,arr,ans,s):
+        return True
+    return False
+
+
+
+3) PRINT ALL WHOSE SUM = K
+https://leetcode.com/problems/combination-sum/
