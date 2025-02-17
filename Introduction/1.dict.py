c0c8b75a85b0ce451a9f5e13c661b3b42403d640
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-17 17:56:01 -0500

    Update 1.dict.py

--- Introduction/1.dict.py
+++ Introduction/1.dict.py
@@ -3,3 +3,16 @@ Method	Use Case
 for k, v in dict.items()	Best for key-value traversal
 for k in dict.keys()	When you only need keys
 for v in dict.values()	When you only need values
+
+
+
+
+  from collections import defaultdict
+
+d = defaultdict(int)
+d = defaultdict(list)
+
+d = defaultdict(lambda:-1)
+d = defaultdict(lambda:0)
+d = defaultdict(lambda:[])
+
