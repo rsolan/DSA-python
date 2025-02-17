bf6f479c55996b0a4dec66fefac35d08c2081aa1
Reema S <144640434+rsolan@users.noreply.github.com>
2025-02-17 18:10:48 -0500

    Update 6.longestSubarray.py

--- 9.Hashmap/6.longestSubarray.py
+++ 9.Hashmap/6.longestSubarray.py
@@ -12,7 +12,13 @@ class Solution:
     #   0 10  15 17 24 25  15 
        
     #   store the first occ
+    # [0,1,2,3, 4,5,6, 7 8]
+    # [2,2,2,-4,1,3,2,-6,6] - arr
+    # [2,4,6, 2,3,6,8,2, 8] - psum - use dict
+    #  1            1
+    # freq dynamically solve --> not precalculated
        
+    #   note len is calucated from next index - ex - for 6-0 = 6 is ans and not 7 ( 1 to 6)
        
         d = defaultdict(int)
         d[0] = -1
@@ -24,13 +30,12 @@ class Solution:
             summ+=nums[ind]   #8
             
             if summ-k in d: #8-6 = 2
-                maxi = max(maxi, ind - d[summ-k] )   #freq of 2 is 3 so cnt should be inc by 3
+                maxi = max(maxi, ind - d[summ-k] )   # 6-0 = 6   , d[2] = 0 as first occ
             
-            if summ in d: #store the first occ
+            if summ in d: #store the first occ if 8 in d ->dont store
                 continue
             else:
-                d[summ] = ind 
+                d[summ] = ind  #-> store the first oc of 8 ie ind = 6
         
         
         return maxi
- 
