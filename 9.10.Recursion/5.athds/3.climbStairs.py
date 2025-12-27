e5db2874b23f5f59d80d64ef103e084988b2859d
Reema S <144640434+rsolan@users.noreply.github.com>
2025-12-27 10:12:15 -0800

    Update 3.climbStairs.py

--- 9.10.Recursion/5.athds/3.climbStairs.py
+++ 9.10.Recursion/5.athds/3.climbStairs.py
@@ -27,3 +27,72 @@ class Solution:
             return memo[n]
 
         return rec(n)
+
+
+
+# 27TH DEC
+
+
+class Solution:
+    # def climbStairs(self, n: int) -> int:
+    #     # n -> n -1 + n-2
+
+    #     def rec(n):
+    #         if n <=1 :
+    #             return 1
+    #         return rec(n-1) + rec(n-2)
+
+
+    #     return rec(n)
+
+
+    # def climbStairs(self, n: int) -> int:
+    #     dp={}
+
+    #     def rec(n):
+    #         if n <=1 :
+    #             return 1
+    #         if n in dp:
+    #             return dp[n]
+    #         dp[n]= rec(n-1) + rec(n-2)
+    #         return dp[n]
+
+
+        # return rec(n)
+
+
+
+
+    # def climbStairs(self, n: int) -> int:
+    #     dp={}
+
+    #     def rec(n):
+    #         if n <=1 :
+    #             return 1
+    #         dp = [0]*(n+1)
+    #         dp[0],dp[1] = 1,1
+
+    #         for i in range(2,n+1):
+    #             dp[i] = dp[i-1]+dp[i-2]
+            
+    #         return dp[n]
+
+
+    #     return rec(n)
+
+    # NOT WORK AS THESE ARE WAYS 
+    def climbStairs(self, n: int) -> int:
+        
+        def rec(n):
+            if n <=1 :
+                return 1
+            a,b = 1,1
+
+            for i in range(2,n+1):
+                a,b = a,a+b
+            
+            return b
+
+
+        return rec(n)
+        
