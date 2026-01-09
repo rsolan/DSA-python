630382cde007161a1873996b04e1e559dda393e0
Reema S <144640434+rsolan@users.noreply.github.com>
2026-01-08 17:49:13 -0800

    Update 5.top-k-frequent-element.py

--- 7.Heap/2.PatternBased/5.top-k-frequent-element.py
+++ 7.Heap/2.PatternBased/5.top-k-frequent-element.py
@@ -26,3 +26,47 @@ class Solution:
             out.append(val)
 
         return out
+
+
+===
+with comemnts
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # d = (i,freq)----imp use dict
+
+        d={}
+        n= len(nums)
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i] = 1
+
+        # print(d) 
+        # {1: 3, 2: 2, 3: 1},   {1: 1} ,    {1: 4, 2: 4, 3: 2}
+
+        heap =[]
+        #make min heap , pop min freq el, keep k max freq el in heap
+
+
+        # for i in d:
+            # print(i,d[i]) 1,3
+
+        for val,freq in d.items():
+            heapq.heappush(heap,(freq,val)) #not v,f
+            if len(heap)>k:
+                heapq.heappop(heap)
+        out=[]
+        while heap:
+            freq,val = heapq.heappop(heap)
+            # we can just traverse also instead of pop-> for frq,val in heap , out.append(v)
+            out.append(val)
+
+        return out
+
+
+
+
+
+
